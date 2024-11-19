d9a1ca2a324f8aed834ab83542d0a47f87f8cac4
Shantanu Sutar <shantanusutar17@gmail.com>
2024-11-19 21:38:42 +0530

    deployment error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist", // Ensure the build output directory is 'dist'
+    sourcemap: true, // Optional: useful for debugging (you can remove this if not needed)
+  },
+});
